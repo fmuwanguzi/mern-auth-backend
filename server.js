c4e875794a05c260b7e01cf0655dd6eccffe0bd8
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-17 12:13:09 -0500

    [server.js] set up and test home route for backend

---
+++ server.js
@@ -0,0 +1,21 @@
+//imports
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const passport = require('passport');
+
+const PORT = process.env.PORT || 8000;
+
+//Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json)
+
+app.get('/', (req,res)=>{
+    res.status(200).json ({ message: 'Smile you are being watched by the backend enginnering team'})
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening 😤 on port: ${PORT}`)
+})
